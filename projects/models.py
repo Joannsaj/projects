e4a3f1d2e1d817816aa01225d2aa740f5d0cb193
joansaj <joannsaj@gmail.com>
2020-11-27 23:18:22 +0300

    added profile model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    profile_pic = CloudinaryField('image', null=True)
+    contact = models.CharField(max_length=30)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(null=True)
+
+    
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()    
+
+    def __str__(self):
+        return self.name
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+
