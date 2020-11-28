4795903d2576d6f34f4f8ab680276cf45a67f2b3
joansaj <joannsaj@gmail.com>
2020-11-29 00:24:10 +0300

    made edits

--- awards/urls.py
+++ awards/urls.py
@@ -19,6 +19,7 @@ from django_registration.backends.one_step.views import RegistrationView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('projects.urls')),
     path('accounts/register/',RegistrationView.as_view(success_url='/profile/'),name='django_registration_register'),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from .models import Project
+from .forms import ProjectForm
 # Create your views here.
-def index():
+def index(request):
     projects = Project.projects()
     return render(request,'index.html',{"projects":projects})
 
-def project():    
+def project(request):    
+    if request.method == 'POST':
+        form = ProjectForm(request.POST,request.FILES)
+        if form.is_valid() :
+            project = form.save(commit=False)
+            project.owner = request.user.profile
+            project.save()
+            return redirect('index')
+    else:
+        form = ProjectForm()
+    return render(request,'project.html',{"form":form})
