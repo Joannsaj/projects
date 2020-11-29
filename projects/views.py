c060af58da59261166f678022a30bc64809d36d6
joansaj <joannsaj@gmail.com>
2020-11-29 16:48:01 +0300

    added profile display

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Project
-from .forms import ProjectForm
+from .forms import ProjectForm, ProfileForm
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
@@ -20,6 +20,7 @@ def project(request):
         form = ProjectForm()
     return render(request,'project.html',{"form":form})
 
+@login_required(login_url='/accounts/login/')
 def search_results(request):
     if 'project' in request.GET and request.GET["project"]:
         search_term = request.GET.get("project")
@@ -29,3 +30,16 @@ def search_results(request):
     else:
         message = "You haven't searched for any term"
         return render(request, 'search.html',{"message":message})
+
+@login_required(login_url='/accounts/login/')
+def profile(request, username):
+    projects = request.user.profile.project.all()
+    if request.method == 'POST':
+        profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if profile_form.is_valid():
+            profile_form.save()
+            return redirect('index')
+    else:
+        profile_form = ProfileForm(instance=request.user.profile)
+
+    return render(request, 'profile.html', { 'profile_form': profile_form, 'projects': projects, })
