7d34f93dee0af4786c0f9458f59bf5e821ae6b98
joansaj <joannsaj@gmail.com>
2020-11-29 18:54:56 +0300

    added serializers

---
+++ projects/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Profile, Project
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('profile_pic', 'contact', 'user', 'bio')
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('title', 'project_image', 'description', 'owner', 'created', 'link')        
